9af57048a9295e5fe53eadcb450f18ca490139f8
Aleksandr <kaaleksandr@vr-protect.ru>
2023-09-29 19:33:08 +0300

    added build for x64, arm,arm64

--- make.cmd
+++ make.cmd
@@ -7,7 +7,17 @@ REM
 
 set GOOS=windows
 set GOARCH=386
-
 echo Building (Windows_x86) dotnetversionsgo.exe
+go build -o bin/x86/dotnetversionsgo.exe -ldflags "-s -w"
 
-go build -o bin/dotnetversionsgo.exe -ldflags "-s -w"
+set GOARCH=amd64
+echo Building (Windows_x64) dotnetversionsgo.exe
+go build -o bin/x64/dotnetversionsgo.exe -ldflags "-s -w"
+
+set GOARCH=arm
+echo Building (Windows_arm) dotnetversionsgo.exe
+go build -o bin/arm/dotnetversionsgo.exe -ldflags "-s -w"
+
+set GOARCH=arm64
+echo Building (Windows_arm64) dotnetversionsgo.exe
+go build -o bin/arm64/dotnetversionsgo.exe -ldflags "-s -w"
